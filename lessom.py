08efb1977f49d6d10162f967091fd648b6536cc5
saba2014 <gurasashvilisaba4@gmail.com>
2025-06-11 18:06:37 +0400

    commit

---
+++ lessom.py
@@ -0,0 +1,32 @@
+if month(1,6,9):
+    print("ზამთარი")
+elif month(3,6,9):
+    print("გაზაფხული")
+elif month (4,8,10):
+    print("ზაფხული")
+elif month(2,4,11):
+    print("შემოდგომა")
+else :
+    print("არასწორი თვე")
+
+    #if ამოწმებს პირობას
+    #else თუ არცერთი პირობა არ არის სწორი 
+    #elif შემდგომი პირობაა
+
+
+
+
+number = 11
+if number>10 :
+    print(მეტია 10-ზე)
+else:
+    print("ნაკლებია 10-ზე")
+
+
+num = 1
+if num > 0:
+    print("მეტია 0-ზე")
+elif num < 0:
+    print("არასწორია")
+else:
+    print("ნულია")
