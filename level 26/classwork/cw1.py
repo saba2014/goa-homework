2bb5ea5d71741d9924be33aaf46360310e3aae64
saba2014 <gurasashvilisaba4@gmail.com>
2025-07-21 17:28:05 +0400

    first commit

--- level 26/classwork/cw1.py
+++ level 26/classwork/cw1.py
@@ -21,9 +21,9 @@ def max_of_two(a , b ) :
 # დაწერე ფუნქცია max_of_three, რომელიც იღებს სამ მთელ რიცხვს და აბრუნებს მათგან უდიდეს
 
 def max_of_three (a, b, c):
-    if a>= b and a >= c:
+    if a > b and a > c:
         return a
-    elif b >= a and b >= c:
+    elif b > a and b > c:
         return b
     else:
         return c
@@ -35,7 +35,7 @@ def max_of_three (a, b, c):
 
 
 def is_hot(temperature):
-    if temperature >= 30:
+    if temperature > 30:
         return True
     else:
         return False
